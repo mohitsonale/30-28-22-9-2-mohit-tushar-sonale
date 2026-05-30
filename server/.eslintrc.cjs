7fee02de2d8559b47cff7527397268047d3ae4ca
vanshrastogi111 <vanshrastogi111@gmail.com>
2026-05-30 15:43:03 +0530

    chore(lint): add ESLint configs and react plugin for client/server

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended'],
+  rules: {},
+};
